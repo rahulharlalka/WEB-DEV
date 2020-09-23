771065352c468d9c4c862c79781c82e9b73dbabf
rahul harlalka <rahulharlalka96@gmail.com>
2020-09-23 13:35:08 +0530

    config of heroku done

--- TODO-v1/app.js
+++ TODO-v1/app.js
@@ -116,6 +116,11 @@ app.get("/about", function (req, res) {
 });
 
 
-app.listen(3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, () => {
     console.log("server ready");
 });
